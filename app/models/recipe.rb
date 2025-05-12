451c55edb3558ab63873e15e5abbda3b6027bd55
vytautas briauka <vytautas.briauka@gmail.com>
2025-05-12 12:23:29 +0100

    some typos fixed

--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -1,6 +1,7 @@
 class Recipe < ApplicationRecord
   has_many :bookmarks
+  
   validates :description, presence: true
-  validates :name, uniqueness: true
+  validates :name, uniqueness: true, presence: true
   validates :rating, length: { in: 0..5 }
 end
